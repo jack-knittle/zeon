fe0547db3b2ef7593a755bc0296a04634e8d0914
Jack Knittle <jknittl@siue.edu>
2025-02-03 11:57:55 -0600

    adjoin generators blade span

--- Zeon/ZeonAlgebra.lean
+++ Zeon/ZeonAlgebra.lean
@@ -33,46 +33,92 @@ lemma gen_pow (n : σ) (k : ℕ) (hk : k ≥ 2) : (generator n (R := R)) ^ k = 0
 
 
 lemma adjoin_generators : Algebra.adjoin R (Set.range (generator : σ → ZeonAlgebra σ R)) = ⊤ := by
-  -- this should be true because the generators are the images of the `X i` polynomials
-  -- under the quotient map, and these polynomials span the `MvPolynomial` ring.
+  have h : Set.range (generator : σ → ZeonAlgebra σ R) = Set.range (Ideal.Quotient.mk _ ∘ X) := by
+    unfold generator
+    ext x
+    simp only [Set.mem_range, Function.comp_apply]
+  rw [h, Set.range_comp, Algebra.adjoin_image (Ideal.Quotient.mk (Ideal.span {x | ∃ i, X i ^ 2 = x}) : MvPolynomial σ R →ₐ[R] Subalgebra R (ZeonAlgebra σ R)) (Set.range X : Set (MvPolynomial σ R)), MvPolynomial.adjoin_range_X]
   sorry
-
-/- unnecessary -/
-@[simp]
-lemma gen_even (n : σ) (k : ℕ) (hk : k ≠ 0): (generator (R := R) n) ^ (2 * k) = 0 := by
-  rw [pow_mul, gen_sq, zero_pow hk]
 
 def blade (s : Finset σ) : ZeonAlgebra σ R := ∏ i in s, generator (R := R) i
 
 lemma blade_empty : blade (R := R) (∅ : Finset σ) = 1 := by
   simp only [blade, Finset.prod_empty]
 
-lemma blade_sq (s : Finset σ) (hs : s ≠ ∅) : blade (R := R) s ^ 2 = 0 := by
-  obtain ⟨i, hi⟩ := Finset.nonempty_of_ne_empty hs
+lemma blade_sq (s : Finset σ) (hs : s.Nonempty) : blade (R := R) s ^ 2 = 0 := by
+  obtain ⟨i, hi⟩ := hs
   rw [blade, ←Finset.prod_pow, Finset.prod_eq_zero hi (gen_sq i)]
 
--- you should phrase `hs` as `s.Nonempty`.
-lemma blade_pow (s : Finset σ) (k : ℕ) (hk : k ≥ 2) (hs : s ≠ ∅) : blade (R := R) s ^ k = 0 := by
+lemma blade_pow (s : Finset σ) (k : ℕ) (hk : k ≥ 2) (hs : s.Nonempty) : blade (R := R) s ^ k = 0 := by
   obtain ⟨i, rfl⟩ := Nat.exists_eq_add_of_le hk
   rw [pow_add, blade_sq s hs, zero_mul]
 
 variable [DecidableEq σ]
 
-/- Not sure if I need two lemmas here or if it can be fit into one -/
-lemma blade_prod_disjoint (s t : Finset σ)  (hst : s ∪ t = ∅): blade (R := R) s * blade t = blade (s ∪ t) := by
-  sorry
+lemma blade_prod_disjoint (s t : Finset σ)  (hst : Disjoint s t): blade (R := R) s * blade t = blade (s ∪ t) := by
+  rw [blade, blade, ←Finset.prod_union hst, ←blade]
 
-lemma blade_prod_inter (s t : Finset σ) (hst : s ∪ t ≠ ∅): blade (R := R) s * blade t = 0 := by
-  sorry
+lemma blade_prod_inter (s t : Finset σ) (hst : ¬Disjoint s t): blade (R := R) s * blade t = 0 := by
+  obtain ⟨i, hi⟩ := Finset.not_disjoint_iff.1 hst
+  rw [blade, blade, ←Finset.prod_erase_mul s generator hi.left, ←Finset.mul_prod_erase t generator hi.right, mul_assoc, ←mul_assoc (generator i), ←sq, gen_sq]
+  simp
+
 
 -- After this we'll want to turn it into a basis using `Basis.mk`, but we'll need to prove linear independence using `LinearIndependent.map`.
 -- `MvPolynomial.linearIndependent_X`
 lemma blade_span (s : Finset σ) : Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) = ⊤ := by
+  have h1 : 1 ∈ Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) := by
+    rw [←blade_empty]
+    apply Submodule.subset_span
+    exact Set.mem_range_self ∅
+
+  have h2 : ∀ (x y : ZeonAlgebra σ R),  x ∈ Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) → y ∈ Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) → x * y ∈ Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) := by
+    have h : Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) = Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R) ∪ {0}) := by
+      rw [Submodule.span_union]
+      have h'' : Set.singleton (0 : ZeonAlgebra σ R) ⊆ Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) := by
+        sorry
+      have h''' : Submodule.span R {0} ≤ Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R)) := by
+        apply Submodule.span_le.2 h''
+      rw [sup_eq_left.mpr h''']
+
+    have h' : ∀ (s t : Finset σ), blade s * blade t ∈ Set.range (blade : Finset σ → ZeonAlgebra σ R) ∪ {0} := by
+      intro s t
+      by_cases hst : Disjoint s t
+      rw [blade_prod_disjoint s t hst]
+      apply Set.mem_union_left
+      exact Set.mem_range_self (s ∪ t)
+      rw [blade_prod_inter s t hst]
+      apply Set.mem_union_right
+      exact Set.mem_singleton 0
+
+    intros x y hx hy
+    rw [h] at hx hy ⊢
+
+    sorry
+
+  have h3 : Set.range (generator : σ → ZeonAlgebra σ R) ⊆ (Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R))).toSubalgebra h1 h2 := by
+    intro x hx
+    obtain ⟨s, rfl⟩ := Set.mem_range.1 hx
+    apply Submodule.subset_span
+    unfold blade
+    apply Set.mem_range.2
+    use {s}
+    rw [Finset.prod_singleton]
+
+  have h4 : Algebra.adjoin R (Set.range (generator : σ → ZeonAlgebra σ R)) ≤ (Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R))).toSubalgebra h1 h2 := by
+    exact Algebra.adjoin_le h3
+
+  have h5 : ⊤ ≤ (Submodule.span R (Set.range (blade : Finset σ → ZeonAlgebra σ R))).toSubalgebra h1 h2 := by
+    rw [←adjoin_generators]
+    exact h4
+
+  exact top_le_iff.1 h5
+
+
   -- this should follow from `adjoin_generators` and the fact that the blades are products of the generators
   -- We want to claim that if `s` is a set closed under multiplication, then so is `span s`, and then we apply
   -- `Submodule.toSubalgebra` to get a subalgebra containing the blades, which hence also contains the generators.
   -- this must be bigger than `Algebra.adjoin R (Set.range (generator : σ → ZeonAlgebra σ R))`, which is the whole algebra.
-  sorry
 
 /- This is wrong but maybe sort of close -/
 def grade_n_part (n : ℕ) (x : ZeonAlgebra σ R) : ZeonAlgebra σ R := ∑ s in Finset.filter (λ s => s.card = n) (Finset.powerset (Finset.univ : Finset σ)), blade s
