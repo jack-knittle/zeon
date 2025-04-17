87426afaf36203bf45d78212265cc21815aa3f23
jack-knittle <jackknittle@gmail.com>
2025-04-17 14:01:12 -0500

    Update Zeon.lean

--- Zeon.lean
+++ Zeon.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `Zeon` library.
 -- Import modules here that should be built as part of the library.
-import Zeon.Basic
+import Zeon.ZeonAlgebra
+import Zeon.Basis
+import Zeon.Inverse
